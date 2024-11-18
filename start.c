e662868fff81815c4c5397d64c71f9c617305e19
Johann Buere <johannbuere@gmail.com>
2024-11-18 17:54:53 +0800

    Fixed heap sorting func

--- start.c
+++ start.c
@@ -11,10 +11,15 @@ GENERAL NOTE: This is only a test code with some of the algo's at the top of my 
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <time.h>
 
+// Menu's
 void mainMenu();
 void linearDSMenu();
 void nonlinDSMenu();
+void stringsMenu();
+void sortingMenu();
+void searchingMenu();
 void treeMenu();
 
 // Sorting Algo's
@@ -23,16 +28,40 @@ void treeMenu();
 void maxHeapify(int arr[], int n, int i, int* iterations);
 void minHeapify(int arr[], int n, int i, int* iterations);
 void heapSort();
+
+// radix Sort
 void countSortRadix(int arr[], int n, int exp, int* iterations);
 void lsdRadixSort(int arr[], int n, int* iterations);
 void radixSort();
+
+// quick Sort
 int lomutoPartition(int arr[], int low, int high, int* iterations);
 int hoarePartition(int arr[], int low, int high, int* iterations);
 void quickSortHelper(int arr[], int low, int high, int* iterations, int partitionType);
 void quickSort();
 
+// Merge Sort
+void mergeSortHelper(int arr[], int left, int right);
+void merge(int arr[], int left, int mid, int right);  
+void mergeSort();
+
+// random sort
+void randSort();          
+
+// insertion sort
+void insertSort();          
+
+ //  Counting Sort
+void countSort();          
+
+// Selection Sort
+void selecSort();           
+
+// Bubble Sort
+void bubbleSort();          
 
 
+// Main code block
 int main() {
     int choice, size, value, key, maxSize, top = -1, front = -1, rear = -1;
     int *arr, *queue, *stack;
@@ -52,13 +81,13 @@ int main() {
                 nonlinDSMenu();
                 break;
             case 3:
-                stringsMenu(); // To be made
+                stringsMenu(); 
                 break;
             case 4:
-                sortingMenu(); // To be made
+                sortingMenu(); 
                 break;
             case 5:
-                searchingMenu(); // To be made
+                searchingMenu(); 
                 break;
             case 6:
                 printf("Exiting program. Goodbye!\n");
@@ -96,7 +125,7 @@ void linearDSMenu() {
         scanf("%d", &choice);
 
         switch (choice) {
-            case 1:
+            /*case 1:
                 arrayMenu(); // To be made
                 break;
             case 2:
@@ -107,7 +136,7 @@ void linearDSMenu() {
                 break;
             case 4:
                 queueMenu(); // To be made
-                break;
+                break;*/
             case 5:
                 printf("Returning to Main Menu...\n");
                 break;
@@ -128,7 +157,7 @@ void nonlinDSMenu() {
 
         switch (choice) {
         case 1:
-            treeMenu(); // To be made
+            treeMenu(); 
             break;
         case 2:
             printf("Returning to Main Menu...\n");
@@ -156,7 +185,7 @@ void stringsMenu(){
         scanf("%d", &choice);
 
         switch (choice) {
-        case 1:
+        /*case 1:
             strGetChar(); // To be made
             break;
         case 2:
@@ -185,7 +214,7 @@ void stringsMenu(){
             break;
         case 10:
             strSort(); // To be made
-            break;
+            break;*/
         case 11:
             printf("Returning to Main Menu...\n");
             break;  
@@ -212,31 +241,31 @@ void sortingMenu(){
 
         switch (choice) {
         case 1:
-            heapSort(); // To be made
+            heapSort(); 
             break;
         case 2:
-            radixSort(); // To be made
+            radixSort(); 
             break;
         case 3:
-            quickSort(); // To be made
+            quickSort();
             break;
         case 4:
-            mergeSort(); // To be made
+            mergeSort(); 
             break; 
         case 5:
-            randSort(); // To be made
+            randSort(); 
             break;
         case 6:
-            insertSort(); // To be made
+            insertSort(); 
             break;
         case 7:
-            countSort(); // To be made
+            countSort(); 
             break;
         case 8:
-            selecSort(); // To be made
+            selecSort(); 
             break;
         case 9:
-            bubbleSort(); // To be made
+            bubbleSort();
             break;
         case 10:
             printf("Returning to Main Menu...\n");
@@ -256,12 +285,12 @@ void searchingMenu(){
         scanf("%d", &choice);
 
         switch (choice) {
-        case 1:
+        /*case 1:
             linSearch(); // To be made
             break;
         case 2:
             binSearch(); // To be made
-            break;
+            break;*/
         case 3:
             printf("Returning to Main Menu...\n");
             break;
@@ -283,7 +312,7 @@ void treeMenu(){
         scanf("%d", &choice);
 
         switch (choice) {
-        case 1:
+        /*case 1:
             showTree(); // To be made
             break;
         case 2:
@@ -294,7 +323,7 @@ void treeMenu(){
             break;
         case 4:
             binaryTree(); // To be made
-            break; 
+            break;*/
         case 5:
             printf("Returning to Non-Linear DS Menu...\n");
             break;
@@ -305,42 +334,48 @@ void treeMenu(){
 
 
 // Heap Sort 
-void maxHeapify(int arr[], int n, int i, int* iterations) {
+#include <stdio.h>
+
+void maxHeapify(int arr[], int n, int i, int *iterations) {
     int largest = i;
     int left = 2 * i + 1;
     int right = 2 * i + 2;
-    (*iterations)++;
+
     if (left < n && arr[left] > arr[largest])
         largest = left;
     if (right < n && arr[right] > arr[largest])
         largest = right;
+
     if (largest != i) {
         int temp = arr[i];
         arr[i] = arr[largest];
         arr[largest] = temp;
+        (*iterations)++;
         maxHeapify(arr, n, largest, iterations);
     }
 }
 
-void minHeapify(int arr[], int n, int i, int* iterations) {
+
+// Heap sort
+void minHeapify(int arr[], int n, int i, int *iterations) {
     int smallest = i;
     int left = 2 * i + 1;
     int right = 2 * i + 2;
-    (*iterations)++;
+
     if (left < n && arr[left] < arr[smallest])
         smallest = left;
     if (right < n && arr[right] < arr[smallest])
         smallest = right;
+
     if (smallest != i) {
         int temp = arr[i];
         arr[i] = arr[smallest];
         arr[smallest] = temp;
+        (*iterations)++;
         minHeapify(arr, n, smallest, iterations);
     }
 }
 
-
-// Radix Sort
 void heapSort() {
     int n, type, iterations = 0;
     printf("Enter the number of elements: ");
@@ -350,9 +385,12 @@ void heapSort() {
     for (int i = 0; i < n; i++)
         scanf("%d", &arr[i]);
 
+    // Ask for Heap Type (Max or Min)
     printf("Choose Heap Type (1 for Max Heap, 2 for Min Heap): ");
     scanf("%d", &type);
+    
 
+    // Build the heap based on the chosen type
     if (type == 1) {
         // Max Heap
         for (int i = n / 2 - 1; i >= 0; i--)
@@ -381,6 +419,7 @@ void heapSort() {
     printf("\nIterations: %d\n", iterations);
 }
 
+//Radix sort
 void countSortRadix(int arr[], int n, int exp, int* iterations) {
     int output[n];
     int count[10] = {0};
@@ -500,4 +539,235 @@ void quickSort() {
     for (int i = 0; i < n; i++)
         printf("%d ", arr[i]);
     printf("\nIterations: %d\n", iterations);
+}
+
+
+// Merge 
+void merge(int arr[], int left, int mid, int right) {
+    int n1 = mid - left + 1;
+    int n2 = right - mid;
+    int L[n1], R[n2];
+
+    for (int i = 0; i < n1; i++) L[i] = arr[left + i];
+    for (int i = 0; i < n2; i++) R[i] = arr[mid + 1 + i];
+
+    int i = 0, j = 0, k = left;
+    while (i < n1 && j < n2) {
+        arr[k++] = (L[i] <= R[j]) ? L[i++] : R[j++];
+    }
+    while (i < n1) arr[k++] = L[i++];
+    while (j < n2) arr[k++] = R[j++];
+}
+
+void mergeSortHelper(int arr[], int left, int right) {
+    if (left < right) {
+        int mid = left + (right - left) / 2;
+        mergeSortHelper(arr, left, mid);
+        mergeSortHelper(arr, mid + 1, right);
+        merge(arr, left, mid, right);
+    }
+}
+
+
+//Merge Sort
+void mergeSort() {
+    int n;
+    printf("Enter the size of the array: ");
+    scanf("%d", &n);
+
+    int arr[n];
+    printf("Enter %d elements: ", n);
+    for (int i = 0; i < n; i++) scanf("%d", &arr[i]);
+
+    mergeSortHelper(arr, 0, n - 1);
+
+    printf("Sorted Array: ");
+    for (int i = 0; i < n; i++) printf("%d ", arr[i]);
+    printf("\n");
+}
+
+// Random Sort
+void randSort() {
+    int n;
+    printf("Enter the size of the array: ");
+    scanf("%d", &n);
+
+    int arr[n];
+    printf("Enter %d elements: ", n);
+    for (int i = 0; i < n; i++) scanf("%d", &arr[i]);
+
+    srand(time(0));
+    int sorted = 0;
+
+    while (!sorted) {
+        // Shuffle array
+        for (int i = 0; i < n; i++) {
+            int randomIndex = rand() % n;
+            int temp = arr[i];
+            arr[i] = arr[randomIndex];
+            arr[randomIndex] = temp;
+        }
+
+        // Check if sorted
+        sorted = 1;
+        for (int i = 1; i < n; i++) {
+            if (arr[i] < arr[i - 1]) {
+                sorted = 0;
+                break;
+            }
+        }
+    }
+
+    printf("Sorted Array: ");
+    for (int i = 0; i < n; i++) printf("%d ", arr[i]);
+    printf("\n");
+}
+
+// Insertion Sort
+void insertSort() {
+    int n;
+    printf("Enter the size of the array: ");
+    scanf("%d", &n);
+
+    int arr[n];
+    printf("Enter %d elements: ", n);
+    for (int i = 0; i < n; i++) scanf("%d", &arr[i]);
+
+    for (int i = 1; i < n; i++) {
+        int key = arr[i];
+        int j = i - 1;
+
+        while (j >= 0 && arr[j] > key) {
+            arr[j + 1] = arr[j];
+            j--;
+        }
+        arr[j + 1] = key;
+    }
+
+    printf("Sorted Array: ");
+    for (int i = 0; i < n; i++) printf("%d ", arr[i]);
+    printf("\n");
+}
+
+// Counting Sort
+void countSort() {
+    int n;
+    printf("Enter the size of the array: ");
+    scanf("%d", &n);
+
+    int arr[n];
+    printf("Enter %d elements: ", n);
+    for (int i = 0; i < n; i++) scanf("%d", &arr[i]);
+
+    // Find max value
+    int max = arr[0];
+    for (int i = 1; i < n; i++) if (arr[i] > max) max = arr[i];
+
+    int count[max + 1];
+    for (int i = 0; i <= max; i++) count[i] = 0;
+
+    for (int i = 0; i < n; i++) count[arr[i]]++;
+
+    for (int i = 1; i <= max; i++) count[i] += count[i - 1];
+
+    int output[n];
+    for (int i = n - 1; i >= 0; i--) {
+        output[count[arr[i]] - 1] = arr[i];
+        count[arr[i]]--;
+    }
+
+    printf("Sorted Array: ");
+    for (int i = 0; i < n; i++) printf("%d ", output[i]);
+    printf("\n");
+}
+
+
+// selection sort
+void selecSort() {
+    int n;
+    printf("Enter the number of elements: ");
+    scanf("%d", &n);
+    
+    int arr[n];
+    printf("Enter the elements:\n");
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+    
+    printf("Initial Array: ");
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+    
+    // Selection Sort Algorithm with display after each iteration
+    for (int i = 0; i < n - 1; i++) {
+        int minIdx = i;
+        for (int j = i + 1; j < n; j++) {
+            if (arr[j] < arr[minIdx]) {
+                minIdx = j;
+            }
+        }
+        // Swap the found minimum element with the first element
+        int temp = arr[minIdx];
+        arr[minIdx] = arr[i];
+        arr[i] = temp;
+        
+        // Display array after the iteration
+        printf("After iteration %d: ", i + 1);
+        for (int k = 0; k < n; k++) {
+            printf("%d ", arr[k]);
+        }
+        printf("\n");
+    }
+
+    printf("Sorted Array: ");
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+}
+
+
+// bubble sort
+void bubbleSort() {
+    int n;
+    printf("Enter the number of elements: ");
+    scanf("%d", &n);
+    
+    int arr[n];
+    printf("Enter the elements:\n");
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+    
+    printf("Initial Array: ");
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
+    
+    // Bubble Sort Algorithm with display after each iteration
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < n - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                // Swap adjacent elements if they are in the wrong order
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+        // Display array after the iteration
+        printf("After iteration %d: ", i + 1);
+        for (int k = 0; k < n; k++) {
+            printf("%d ", arr[k]);
+        }
+        printf("\n");
+    }
+
+    printf("Sorted Array: ");
+    for (int i = 0; i < n; i++) {
+        printf("%d ", arr[i]);
+    }
+    printf("\n");
 }
